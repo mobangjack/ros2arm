86d1c1ef504b5d9b5dd3233c9d7e284996ed8d5d
mobangjack <mobangjack@foxmail.com>
2021-05-01 04:03:41 +0800

    Fix dependencies

--- build.sh
+++ build.sh
@@ -7,11 +7,18 @@ export ANDROID_TOOLCHAIN=clang
 
 ${PYTHON3_EXEC} generate_package_xml.py base.yml
 
+sed -i "s/libssl-dev//g" src/eProsima/Fast-DDS/package.xml
+sed -i "s/ rt//g" src/eProsima/Fast-DDS/cmake/modules/FindThirdpartyBoost.cmake
+
 touch \
   src/ros2/examples/rclpy/COLCON_IGNORE \
   src/ros2/rcl/rcl/test/COLCON_IGNORE \
   src/ros2/rcl_interfaces/test_msgs/COLCON_IGNORE \
-  src/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
+  src/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE \
+  src/ros-tracing/ros2_tracing/tracetools_launch/COLCON_IGNORE \
+  src/ros-tracing/ros2_tracing/tracetools_read/COLCON_IGNORE \
+  src/ros-tracing/ros2_tracing/tracetools_test/COLCON_IGNORE \
+  src/ros-tracing/ros2_tracing/tracetools_trace/COLCON_IGNORE
 
 colcon build \
     --merge-install \
@@ -28,10 +35,8 @@ colcon build \
         -DCOMPILE_EXAMPLES=OFF \
         -DBUILD_TESTING:BOOL=OFF \
         -DBUILD_MEMORY_TESTS=OFF \
-        -DBUILD_MEMORY_TOOLS=OFF \
         -DBUILD_MEMORY_EXAMPLES=OFF \
         -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF\
-        -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF \
         -DFOONATHAN_MEMORY_BUILD_TESTS=OFF \
         -DBUILD_SHARED_LIBS=ON \
         -DCMAKE_FIND_ROOT_PATH="$PWD/install"
